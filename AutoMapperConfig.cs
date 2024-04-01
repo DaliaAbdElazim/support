d377e4eec16f77927109938bab538c396ba73f07
Dalia Abd Elazim <104151989+DaliaAbdElazim@users.noreply.github.com>
2024-04-01 02:07:48 +0200

    Add files via upload

---
+++ AutoMapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ProjectMiniShop.Models;
+using ProjectMiniShop.Models.DTO;
+
+namespace ProjectMiniShop
+{
+    public class AutoMapperConfig:Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Product,ProductDTO>().ReverseMap();
+        }
+    }
+}
